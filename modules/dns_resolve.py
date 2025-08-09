95a54a9dd3e2695c65cc2e696d5daf5db6370466
DavidBab <davidbab20@gmail.com>
2025-08-09 12:53:59 +0200

    dns-resolve built, requires testing

---
+++ modules/dns_resolve.py
@@ -0,0 +1,37 @@
+import dns.resolver
+
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RED = "\033[91m"
+CYAN = "\033[96m"
+RESET = "\033[0m"
+
+def dns_records(target_domain, rec_type = None):
+    record_types = ["A", "AAAA", "CNAME", "MX", "TXT", "SOA"]
+    resolver = dns.resolver.Resolver()
+
+    if rec_type:
+        if rec_type == "all":
+            for rec_type in record_types:
+                dns_check(domain=target_domain, record=rec_type, resolver=resolver)
+
+        else:
+            dns_check(domain=target_domain, record=rec_type, resolver=resolver)
+
+
+def dns_check(domain, record, resolver):
+    try:
+        answer = resolver.resolve(domain, record)
+        print(f"{record} records for {domain}.")
+        visualize(answer=answer)
+
+    except dns.resolver.NoAnswer:
+        print(f"{RED}[!]{RESET} Sorry, couldn't find any record for {record}.")
+
+    except Exception as e:
+        print(f"An error occured: {e}")
+
+
+def visualize(answer):
+    for data in answer:
+        print(f"{GREEN}[+]{RESET} {data}")
